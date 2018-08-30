6db928da57ed8ff5808deb8fc7af34730dee360e
Akito Ito <akito_ito@r.recruit.co.jp>
2018-08-30 15:16:20 +0900

    Add ideal case

--- e2e/usernotfound.go
+++ e2e/usernotfound.go
@@ -1,1 +1,30 @@
 package e2e
+
+import (
+	"fmt"
+)
+
+//go:generate -Type=userNotFound
+type userNotFound interface {
+	UserNotFound() (id int64)
+}
+
+func IsUserNotFound(err error) (bool, int64) {
+	var id int64
+	if e, ok := err.(userNotFound); ok {
+		return true, e.UserNotFound()
+	}
+	return false, id
+}
+
+type UserNotFound struct {
+	Id int64
+}
+
+func (u *UserNotFound) UserNotFound() (id int64) {
+	return id
+}
+
+func (u *UserNotFound) Error() string {
+	return fmt.Sprintf("userNotFound with Id: %v", u.Id)
+}
