158a2de23ef5095a262f24ffdb32ad8e9a02b105
Akito Ito <akito0107@gmail.com>
2018-09-08 17:48:47 +0900

    chore: update Readme

--- e2e/usernotfound.go
+++ e2e/usernotfound.go
@@ -1,16 +1,16 @@
 package e2e
 
-//go:generate generr -t=userNotFound -i
+//go:generate generr -t userNotFound -i
 type userNotFound interface {
 	UserNotFound() (id int64)
 }
 
-//go:generate generr -t=notFound -i
+//go:generate generr -t notFound -i
 type notFound interface {
 	NotFound()
 }
 
-//go:generate generr -t=emailNotFound -i -m "email %s is not found"
+//go:generate generr -t emailNotFound -i -m "email %s is not found"
 type emailNotFound interface {
 	EmailNotFound() (email string)
 }
